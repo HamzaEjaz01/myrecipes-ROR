405134f6b6fd5b9872d82758046e6634a4679cbc
HamzaEjaz420 <hamzaejaz97@gmail.com>
2021-10-01 08:53:10 -0400

    recipes updated

--- db/migrate/20210930113959_add_name_to_recipes.rb
+++ db/migrate/20210930113959_add_name_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddNameToRecipes < ActiveRecord::Migration[6.1]
   def change
-    add_column :recipes, :name, :string
+    add_column :recipes, :name, :string unless column_exists? :recipes, :name
   end
 end
--- db/migrate/20210930114118_add_email_to_recipes.rb
+++ db/migrate/20210930114118_add_email_to_recipes.rb
@@ -1,5 +1,5 @@
 class AddEmailToRecipes < ActiveRecord::Migration[6.1]
   def change
-    add_column :recipes, :email, :string
+    add_column :recipes, :email, :string unless column_exists? :recipes, :email
   end
 end
